b262ffe4850fcff4378b4a542e8ac2cfb75d905f
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2022-03-05 20:46:39 +0530

    Controller with GET and POST request

--- springboot-backend/src/main/java/in/codinggyan/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/in/codinggyan/springboot/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package in.codinggyan.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +21,15 @@ public class EmployeeController {
 	private EmployeeRepository employeeRepository;
 	
 	//get all employees
-	
+	@CrossOrigin
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-
+	
+	@CrossOrigin
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+		}
 }
